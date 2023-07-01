4ddac74bb41d0565d42eb5e360c837b8107ecd4f
Moritz Boos <m.boos@eyeo.com>
2023-07-01 16:16:42 +0100

    improved cmd

--- ultralight_hiking/cmd.py
+++ ultralight_hiking/cmd.py
@@ -9,16 +9,13 @@ app = typer.Typer()
 
 @app.command()
 def main(
-    user_agent: Optional[str]= typer.Argument(None),
+    user_agent: Optional[str] = typer.Argument(None),
     client_secret: Optional[str] = typer.Argument(None),
     client_id: Optional[str] = typer.Argument(None),
-    after: datetime = typer.Option(
-        datetime.today(), formats=["%Y-%m-%d", "%Y/%m/%d"]
-    ),
-    before: datetime = typer.Option(
-        datetime.today() + timedelta(days=1), formats=["%Y-%m-%d", "%Y/%m/%d"]
-    ),
-    prefix: str = typer.Option(int(datetime.today().timestamp()))
+    after: datetime = typer.Option(datetime.now() - timedelta(hours=24)),
+    before: datetime = typer.Option(datetime.now()),
+    prefix: str = typer.Option(datetime.today().strftime("%Y-%m-%d")),
+    max_cache: Optional[int] = typer.Option(None),
 ):
     if user_agent is None:
         user_agent = os.environ.get("REDDIT_USER_AGENT")
@@ -38,16 +35,18 @@ def main(
             raise ValueError(
                 "Please provide a client id or set `REDDIT_CLIENT_ID` environment variable."
             )
-
     submissions_tr = extract.crawl_trip_reports(
         user_agent=user_agent,
         client_id=client_id,
         client_secret=client_secret,
         after=after,
         before=before,
+        max_cache=max_cache,
     )
     if submissions_tr:
-        reports, lp_contents = extract.extract_and_aggregate_submissions(submissions_tr)
+        reports, lp_contents = extract.extract_and_aggregate_submissions(
+            list(submissions_tr)
+        )
         extract.write_jsonl(f"{prefix}-reports.jsonl", reports)
         extract.write_jsonl(f"{prefix}-lp_contents.jsonl", lp_contents)
     else:
